ea37c71f2a508d06a280117817eb451e6b3b0ab1
snowfox <souza.enrique7@gmail.com>
2025-09-24 21:09:51 -0300

    Atualiza settings.py com os django-apps

--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'seaborn',
+    #apps
+    'data_process',
+    'data_visual',
 ]
 
 MIDDLEWARE = [
@@ -120,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / "data_visual/static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
