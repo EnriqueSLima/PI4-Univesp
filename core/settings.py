523ef0c4f307d8e480b7b70ea68312983281af3c
snowfox <souza.enrique7@gmail.com>
2025-11-06 19:46:39 -0300

    Altera vairavel DB

--- core/settings.py
+++ core/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
+        default=os.environ.get('DATABASE_PUBLIC_URL'),
         conn_max_age=600,
         conn_health_checks=True,
         ssl_require=True
